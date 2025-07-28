MISC 1. remove windowed objective in ssipp 2. fix bug in heuristic computation 3. add a bunch of (conditional) logging

run_lifelong.py
@@ -241,7 +241,8 @@ def run_lifelong_argos(
         (sim_window_tick / ticks_per_second) * (velocity / 100)).astype(int)
     if planner in ["RHCR"]:
         plan_window_ts = np.max([plan_window_ts, planning_window])
-    logger.info(f"Planning window in timesteps: {plan_window_ts}")
+        logger.info(
+            f"{planner}: Planning window in timesteps: {plan_window_ts}")
     # plan_window_ts = int(np.ceil(1.5 * look_ahead_dist * (velocity / 100)))
 
     # Path to the executables